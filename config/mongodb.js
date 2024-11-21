03655ed1f159b8bce596c4d87bd6b8bf90490a59
Ridin Kakkoth <k.ridin@ymail.com>
2024-11-21 15:33:06 +0530

    bug fix

--- config/mongodb.js
+++ config/mongodb.js
@@ -5,7 +5,6 @@ const connectDB=async()=>{
     
     mongoose.connection.on('connected',()=>console.log('Database connected'))
 
-    // await mongoose.connect("mongodb+srv://ridinksr:Mw2WsTtxSKgsgOjO@cluster0.bdd03.mongodb.net/instagram?retryWrites=true&w=majority&appName=Cluster0")
     await mongoose.connect(`${process.env.MONGODB_URI}`)
 }
 
